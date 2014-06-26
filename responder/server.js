732bb48b3fed7b218ee3a0daa0f9cd37b1ea61cc
TQ White II <tq@justkidding.com>
2014-06-26 12:37:32 -0500

    added /StudentPersonal static test endpoint

--- responder/server.js
+++ responder/server.js
@@ -19,22 +19,54 @@ if (program.background) {
 	qtools.die('background not yet implemented');
 }
 
-app.use(bodyParser.urlencoded({extended: true}))
+var transferPacket=require(__dirname+'/specs/transferPacket.js');
+transferPacket=new transferPacket();
+var testData=require(__dirname+'/specs/testData.js');
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+
 app.use(bodyParser.json())
 
 var router = express.Router();
 
 router.get('/', function(req, res) {
 
-	res.json({status:'hello', body:req.body, query:req.query});
+	res.json({
+		status: 'hello',
+		body: req.body,
+		query: req.query
+	});
 });
 
 router.post('/', function(req, res) {
-	res.json({status:'hello', body:req.body, query:req.query});
+	res.json({
+		status: 'hello',
+		body: req.body,
+		query: req.query
+	});
 });
 
 app.use('/ping', router);
 
+var router2 = express.Router();
+
+router2.get('/', function(req, res) {
+	transferPacket.reset().add('Meta', {body: req.body, query: req.query}).add('Data', {StudentPersonal:testData.StudentPersonal});
+	res.json(transferPacket.finishedObject());
+});
+
+// router2.post('/', function(req, res) {
+// 	res.json({
+// 		status: 'school',
+// 		body: req.body, query: req.query
+// 	});
+// });
+
+app.use('/StudentPersonal', router2);
+
 app.listen(config.port);
 qtools.message('Magic happens on port ' + config.port);
+
 
