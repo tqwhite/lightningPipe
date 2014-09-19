3b4bd7449c69729712bf361e4cca8ad5e662f952
TQ White II <tq@justkidding.com>
2014-09-19 14:58:23 -0500

    reorged management folder, added test, prod scripting

---
+++ system/management/cloverleaf/devMac/common/accessLpTestJs/getAllIndividualFromLpTestJs/0676.js
@@ -0,0 +1,7 @@
+
+
+[
+{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/0676/schools/0676_010/segments/Guardian?sendFlatSpecs=true","destination":"0676/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/0676/schools/0676_010/segments/Schedule?sendFlatSpecs=true","destination":"0676/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/0676/schools/0676_010/segments/Student_Enrolled?sendFlatSpecs=true","destination":"0676/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+]
--- zDiscardEventually/originalSystem/configINI/localEnvironment.js
+++ zDiscardEventually/originalSystem/configINI/localEnvironment.js
@@ -29,7 +29,7 @@ var moduleFunction = function(args) {
 	this.sendMetaData = true;
 	this.testServer = true;
 	
-	this.dataFileDirectory='/Volumes/qubuntuFileServer/cmerdc/lightningPipe/dataFiles/';
+	this.dataFileDirectory='/Volumes/qubuntuFileServer/cmerdc/lightningPipe/testDataFiles/';
 
 
 	
