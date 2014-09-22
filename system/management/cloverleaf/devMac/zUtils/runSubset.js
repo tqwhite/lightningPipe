3907531e3b9d7980e5520a466b87c833adcaf222
TQ White II <tq@justkidding.com>
2014-09-22 16:14:11 -0500

    reorg'd management scripts, added throttling to cloverleaf. it now only pushes a specified number of calls to lightning pipe at a time.

---
+++ system/management/cloverleaf/devMac/zUtils/runSubset.js
@@ -0,0 +1,35 @@
+[
+
+{"source":"http://localhost:8081/uff/1.0/districts/0146/schools/0146_010/segments/Guardian?sendFlatSpecs=true","destination":"0146/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0146/schools/0146_040/segments/Guardian?sendFlatSpecs=true","destination":"0146/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0150/schools/0150_010/segments/Guardian?sendFlatSpecs=true","destination":"0150/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0150/schools/0150_011/segments/Guardian?sendFlatSpecs=true","destination":"0150/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0150/schools/0150_020/segments/Guardian?sendFlatSpecs=true","destination":"0150/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0487/schools/0487_010/segments/Guardian?sendFlatSpecs=true","destination":"0487/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0676/schools/0676_010/segments/Guardian?sendFlatSpecs=true","destination":"0676/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0741/schools/0741_001/segments/Guardian?sendFlatSpecs=true","destination":"0741/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0741/schools/0741_002/segments/Guardian?sendFlatSpecs=true","destination":"0741/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0741/schools/0741_020/segments/Guardian?sendFlatSpecs=true","destination":"0741/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0745/schools/0745_010/segments/Guardian?sendFlatSpecs=true","destination":"0745/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0745/schools/0745_030/segments/Guardian?sendFlatSpecs=true","destination":"0745/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0745/schools/0745_035/segments/Guardian?sendFlatSpecs=true","destination":"0745/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0745/schools/0745_050/segments/Guardian?sendFlatSpecs=true","destination":"0745/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0745/schools/0745_060/segments/Guardian?sendFlatSpecs=true","destination":"0745/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0768/schools/0768_010/segments/Guardian?sendFlatSpecs=true","destination":"0768/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0801/schools/0801_010/segments/Guardian?sendFlatSpecs=true","destination":"0801/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0801/schools/0801_020/segments/Guardian?sendFlatSpecs=true","destination":"0801/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0803/schools/0803_001/segments/Guardian?sendFlatSpecs=true","destination":"0803/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0803/schools/0803_002/segments/Guardian?sendFlatSpecs=true","destination":"0803/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/0914/schools/0914_001/segments/Guardian?sendFlatSpecs=true","destination":"0914/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/2769/schools/2769_010/segments/Guardian?sendFlatSpecs=true","destination":"2769/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/2769/schools/2769_030/segments/Guardian?sendFlatSpecs=true","destination":"2769/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/2888/schools/2888_001/segments/Guardian?sendFlatSpecs=true","destination":"2888/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/2888/schools/2888_003/segments/Guardian?sendFlatSpecs=true","destination":"2888/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/2888/schools/2888_006/segments/Guardian?sendFlatSpecs=true","destination":"2888/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/6026/schools/6026_020/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/6026/schools/6026_024/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://localhost:8081/uff/1.0/districts/6026/schools/6026_025/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}}
+
+
+
+]
