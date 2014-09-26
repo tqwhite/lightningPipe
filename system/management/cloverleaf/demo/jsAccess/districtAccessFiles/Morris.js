6fea7d7e1c8c79cbc2189ca83a07b07ad09c6456
TQ White II <tq@justkidding.com>
2014-09-26 14:04:23 -0500

    more management file

--- system/management/cloverleaf/demo/jsAccess/districtAccessFiles/Morris.js
+++ system/management/cloverleaf/demo/jsAccess/districtAccessFiles/Morris.js
@@ -1,9 +1,9 @@
 [
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Guardian/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Schedule/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Student_Enrolled/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Schedule","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Student_Enrolled","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
 
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Guardian/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Schedule/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Student_Enrolled/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Schedule","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Student_Enrolled","destination":"/home/lightningpipe/mountPoint/destDirDemo/Morris/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
 ]
--- system/management/cloverleaf/demo/jsAccess/districtAccessFiles/WCE.js
+++ system/management/cloverleaf/demo/jsAccess/districtAccessFiles/WCE.js
@@ -1,17 +1,17 @@
 [
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/020/segments/Guardian/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/020/segments/Schedule/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/020/segments/Student_Enrolled/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/020","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/020/segments/Schedule","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/020/segments/Student_Enrolled","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
 
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/023/segments/Guardian/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/023/segments/Schedule/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/023/segments/Student_Enrolled/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/023","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/023/segments/Schedule","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/023/segments/Student_Enrolled","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
 
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/024/segments/Guardian/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/024/segments/Schedule/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/024/segments/Student_Enrolled/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/024","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/024/segments/Schedule","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/024/segments/Student_Enrolled","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
 
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/025/segments/Guardian/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/025/segments/Schedule/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/025/segments/Student_Enrolled/segments/Guardian","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/025","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/025/segments/Schedule","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/WCE/schools/025/segments/Student_Enrolled","destination":"/home/lightningpipe/mountPoint/destDirDemo/WCE/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
 ]
