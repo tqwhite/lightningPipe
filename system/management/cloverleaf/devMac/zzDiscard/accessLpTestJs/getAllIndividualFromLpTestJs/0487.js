1124e959846a1c56d9b197d1fdafcc47b68499cb
TQ White II <tq@justkidding.com>
2014-09-26 09:24:43 -0500

    renamed test.api to demo.api'

--- system/management/cloverleaf/devMac/zzDiscard/accessLpTestJs/getAllIndividualFromLpTestJs/0487.js
+++ system/management/cloverleaf/devMac/zzDiscard/accessLpTestJs/getAllIndividualFromLpTestJs/0487.js
@@ -2,7 +2,7 @@
 
 [
 
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/0487/schools/0487_010/segments/Guardian?sendFlatSpecs=true","destination":"0487/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/0487/schools/0487_010/segments/Schedule?sendFlatSpecs=true","destination":"0487/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/0487/schools/0487_010/segments/Student_Enrolled?sendFlatSpecs=true","destination":"0487/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/0487/schools/0487_010/segments/Guardian?sendFlatSpecs=true","destination":"0487/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/0487/schools/0487_010/segments/Schedule?sendFlatSpecs=true","destination":"0487/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/0487/schools/0487_010/segments/Student_Enrolled?sendFlatSpecs=true","destination":"0487/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
 ]
--- system/management/cloverleaf/devMac/zzDiscard/accessLpTestJs/getAllIndividualFromLpTestJs/6026.js
+++ system/management/cloverleaf/devMac/zzDiscard/accessLpTestJs/getAllIndividualFromLpTestJs/6026.js
@@ -1,15 +1,15 @@
 
 
 [
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_020/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_020/segments/Schedule?sendFlatSpecs=true","destination":"6026/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_020/segments/Student_Enrolled?sendFlatSpecs=true","destination":"6026/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_024/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_024/segments/Schedule?sendFlatSpecs=true","destination":"6026/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_024/segments/Student_Enrolled?sendFlatSpecs=true","destination":"6026/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_025/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_025/segments/Schedule?sendFlatSpecs=true","destination":"6026/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
-{"source":"http://test.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_025/segments/Student_Enrolled?sendFlatSpecs=true","destination":"6026/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_020/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_020/segments/Schedule?sendFlatSpecs=true","destination":"6026/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_020/segments/Student_Enrolled?sendFlatSpecs=true","destination":"6026/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_024/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_024/segments/Schedule?sendFlatSpecs=true","destination":"6026/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_024/segments/Student_Enrolled?sendFlatSpecs=true","destination":"6026/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_025/segments/Guardian?sendFlatSpecs=true","destination":"6026/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_025/segments/Schedule?sendFlatSpecs=true","destination":"6026/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/6026/schools/6026_025/segments/Student_Enrolled?sendFlatSpecs=true","destination":"6026/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
 
 
 
