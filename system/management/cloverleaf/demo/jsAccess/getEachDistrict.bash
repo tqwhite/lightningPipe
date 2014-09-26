ee7453f11e5a6203e4f2bc6dafb746edb38a26ec
TQ White II <tq@justkidding.com>
2014-09-26 14:35:07 -0500

    more management file

--- system/management/cloverleaf/demo/jsAccess/getEachDistrict.bash
+++ system/management/cloverleaf/demo/jsAccess/getEachDistrict.bash
@@ -1,8 +1,38 @@
 #!/bin/sh
 
+export DISTRICTNAME='Albany';
+
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+$cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+
+export DISTRICTNAME='Clinton-Graceville';
+
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+$cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+
+export DISTRICTNAME='Hancock';
+
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+$cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+
+export DISTRICTNAME='Morris';
+
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+$cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+
+export DISTRICTNAME='Paynesville';
+
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+$cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+
 export DISTRICTNAME='WCE';
 
-rm -r /home/mountPoint/destDirDemo/$DISTRICTNAME/*;
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+$cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+
+export DISTRICTNAME='Wheaton';
+
+rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
 
 
