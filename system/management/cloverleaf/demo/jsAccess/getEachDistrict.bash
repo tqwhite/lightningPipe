bc7b1e56c7ec8cf381c5ecaf4cdf05ed3ed00ca6
TQ White II <tq@justkidding.com>
2014-09-26 14:52:39 -0500

    more management file

--- system/management/cloverleaf/demo/jsAccess/getEachDistrict.bash
+++ system/management/cloverleaf/demo/jsAccess/getEachDistrict.bash
@@ -3,36 +3,50 @@
 export DISTRICTNAME='Albany';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 export DISTRICTNAME='Clinton-Graceville';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 export DISTRICTNAME='Hancock';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 export DISTRICTNAME='Morris';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 export DISTRICTNAME='Paynesville';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 export DISTRICTNAME='WCE';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 export DISTRICTNAME='Wheaton';
 
 rm -r /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/*;
+echo "/nStarting $DISTRICTNAME";
 $cloverDemo -f ~/lpDemo/services/system/management/cloverleaf/demo/jsAccess/districtAccessFiles/$DISTRICTNAME.js
+ls -la /home/lightningpipe/mountPoint/destDirDemo/$DISTRICTNAME/
 
 
