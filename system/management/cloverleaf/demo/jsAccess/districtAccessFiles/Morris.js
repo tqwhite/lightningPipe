50df30e2ef103ffc818f3c707fcb0f9a2555451c
TQ White II <tq@justkidding.com>
2014-09-26 11:18:51 -0500

    working out operational files

---
+++ system/management/cloverleaf/demo/jsAccess/districtAccessFiles/Morris.js
@@ -0,0 +1,9 @@
+[
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Guardian/segments/Guardian","destination":"~/mountPoint/destDirDemo/Morris/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Schedule/segments/Guardian","destination":"~/mountPoint/destDirDemo/Morris/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/010/segments/Student_Enrolled/segments/Guardian","destination":"~/mountPoint/destDirDemo/Morris/Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Guardian/segments/Guardian","destination":"~/mountPoint/destDirDemo/Morris/Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Schedule/segments/Guardian","destination":"~/mountPoint/destDirDemo/Morris/Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://demo.api.lightningpipe.org/uff/1.0/districts/Morris/schools/030/segments/Student_Enrolled/segments/Guardian","destination":"~/mountPoint/destDirDemo/Morris/Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+]
