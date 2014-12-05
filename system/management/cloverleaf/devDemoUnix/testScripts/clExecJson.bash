dae3910bded3f77fbf68ce800633771aca9db092
TQ White II <tq@justkidding.com>
2014-12-05 14:57:50 -0600

    tuning up scripts

--- system/management/cloverleaf/devDemoUnix/testScripts/clExecJson.bash
+++ system/management/cloverleaf/devDemoUnix/testScripts/clExecJson.bash
@@ -4,6 +4,6 @@ echo "clearing HawleyMinus/StudentEnrolled.txt";
 rm $clTestDestDir/HawleyMinus/StudentEnrolled.txt;
 
 echo "execute json";
-$cloverleaf -f /Volumes/qubuntuFileServer/cmerdc/lightningPipe/system/management/cloverleaf/devMac/testScripts/testJson.js
+$cloverleaf -f /Volumes/qubuntuFileServer/cmerdc/lightningPipe/system/management/cloverleaf/devDemoUnix//testScripts/testJson.js
 
 ls -la /Volumes/qubuntuFileServer/cmerdc/lightningPipe/testResults/HawleyMinus;
