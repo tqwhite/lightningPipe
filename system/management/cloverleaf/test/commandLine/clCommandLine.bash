7044baa799fb6c52311c0a1a76b1be77683608ae
TQ White II <tq@justkidding.com>
2014-12-12 15:22:28 -0600

    more test scripting

--- system/management/cloverleaf/test/commandLine/clCommandLine.bash
+++ system/management/cloverleaf/test/commandLine/clCommandLine.bash
@@ -9,8 +9,6 @@
 # http://127.0.0.1:8000/uff/1.0/districts/HawleyMinus/schools/011/segments/Schedule#Data?sendFlatSpecs=true \
 # $destDirProd/HawleyMinus/Schedules.txt -ah
 
-echo -e "\nStart CURL  ==============";
-
 if [[ -z "$1" ]]; then
 segment="Student_Enrolled"
 else
@@ -18,68 +16,61 @@ segment=$1
 fi
 
 if [[ -z "$2" ]]; then
-district="Albany"
+school="030"
 else
-district=$2
+school=$2
 fi
 
 if [[ -z "$3" ]]; then
-school="030"
+district="Albany"
 else
-school=$3
+district=$3
 fi
+
+
+if [[ -z "$1" ]]; then
+
+echo -e "\nUsage:\n\
+clcmdtest segmentName schoolName districtName\n\
+default: clcmdtest StudentEnrolled 030 Albany\n\
+"
+
+else
+
+echo -e "\nStart Cloverleaf run  ==============";
+
 
 echo -e "\nclearing $district/$segment (for school $school)";
 rm $testDestDir/"$district"CmdLine/$segment.txt;
 #ls -la $testDestDir/HawleyMinus;
 
-echo -e "\nExecute cloverleaf";
-
-
-
-if [[ -z "$1" ]]; then
-
-
-
 segment="Student_Enrolled"
-echo "$district/schools/$school/segments/$segment\n"
+echo -e "\ncloverleaf $district/schools/$school/segments/$segment\n"
 
 $cloverleaf \
 http://127.0.0.1:8000/uff/1.0/districts/$district/schools/$school/segments/$segment#Data?sendFlatSpecs=true \
 $testDestDir/"$district"CmdLine/$segment.txt -ah
 
 segment="Schedule"
-echo "$district/schools/$school/segments/$segment\n"
+echo -e "\ncloverleaf $district/schools/$school/segments/$segment\n"
 
 $cloverleaf \
 http://127.0.0.1:8000/uff/1.0/districts/$district/schools/$school/segments/$segment#Data?sendFlatSpecs=true \
 $testDestDir/"$district"CmdLine/$segment.txt -ah
 
 segment="Guardian"
-echo "$district/schools/$school/segments/$segment\n"
+echo -e "\ncloverleaf $district/schools/$school/segments/$segment\n"
 
 $cloverleaf \
 http://127.0.0.1:8000/uff/1.0/districts/$district/schools/$school/segments/$segment#Data?sendFlatSpecs=true \
 $testDestDir/"$district"CmdLine/$segment.txt -ah
 
-else
+echo -e "\nls -la $testDestDir/AlbanyCmdLine;\n"
+ls -la $testDestDir/AlbanyCmdLine;
 
-
-echo "$cloverleaf \
-http://127.0.0.1:8000/uff/1.0/districts/$district/schools/$school/segments/$segment#Data?sendFlatSpecs=true \
-$testDestDir/"$district"CmdLine/$segment.txt -ah\n"
-
-$cloverleaf \
-http://127.0.0.1:8000/uff/1.0/districts/$district/schools/$school/segments/$segment#Data?sendFlatSpecs=true \
-$testDestDir/"$district"CmdLine/$segment.txt -ah
-
+echo -e "\nDone  ==============\n";
 
 fi
 
 
-echo -e "ls -la $testDestDir/AlbanyCmdLine;\n"
-ls -la $testDestDir/AlbanyCmdLine;
 
-
-
-echo -e "\nDone  ==============\n";
