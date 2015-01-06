df1e4788de4be2e51e44ad82f405bec62797752e
TQ White II <tq@justkidding.com>
2015-01-06 11:24:08 -0600

    fixed mispelling

--- system/management/utility/tqScripting/locationScripts/demoLocations.bash
+++ system/management/utility/tqScripting/locationScripts/demoLocations.bash
@@ -5,6 +5,6 @@ export lightningPipeBase="/home/lightningpipe/lpDemo/services/"
 echo "Locations initialized: $homeDir"
 
 #cloverleaf and lightningpath aliases and stuff
-if [ -f /home/lightningpipe/lpDemo/services/system/management/utility/tqScripting/bash_alias_cloverleaf ]; then
-source /home/lightningpipe/lpDemo/services//system/management/utility/tqScripting/bash_alias_cloverleaf
+if [ -f /home/lightningpipe/lpDemo/services/system/management/utility/tqScripting/bash_alias_cloverleaf_demo ]; then
+source /home/lightningpipe/lpDemo/services//system/management/utility/tqScripting/bash_alias_cloverleaf_demo
 fi
