9bbb469518ba2ce1270258bc9c03dce6ba0d6e10
TQ White II <tq@justkidding.com>
2015-01-21 20:45:13 -0600

    incoming data is collected into data buffers and passed to file write at the end

--- system/management/test2/parameterFiles/testJsonDemo.js
+++ system/management/test2/parameterFiles/testJsonDemo.js
@@ -1,22 +1,29 @@
 {
-    "input": [{
-            "source": "http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Guardian?sendFlatSpecs=true",
-            "destination": "Guardians.txt",
-            "path": "Data",
-            "switches": {
-                "header": false
-            }
-        },
+    "input": [
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Guardian?sendFlatSpecs=true","destination":"Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Schedule?sendFlatSpecs=true","destination":"Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
 
-        {
-            "source": "http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Guardian?sendFlatSpecs=true",
-            "destination": "Guardians2.txt",
-            "path": "Data",
-            "switches": {
-                "header": false
-            }
-        }
-    ],
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/030/segments/Guardian?sendFlatSpecs=true","destination":"Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/030/segments/Schedule?sendFlatSpecs=true","destination":"Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/030/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/035/segments/Guardian?sendFlatSpecs=true","destination":"Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/035/segments/Schedule?sendFlatSpecs=true","destination":"Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/035/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/050/segments/Guardian?sendFlatSpecs=true","destination":"Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/050/segments/Schedule?sendFlatSpecs=true","destination":"Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/050/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/060/segments/Guardian?sendFlatSpecs=true","destination":"Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/060/segments/Schedule?sendFlatSpecs=true","destination":"Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/060/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}},
+
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/099/segments/Guardian?sendFlatSpecs=true","destination":"Guardians.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/099/segments/Schedule?sendFlatSpecs=true","destination":"Schedules.txt", "path":"Data", "switches":{"header":false, "append":true}},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/099/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
+],
 
     "process": {
         "type": "passThrough"
