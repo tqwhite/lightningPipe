367ce72605fdc29a2e5e2a459faca741c9b818b3
TQ White II <tq@justkidding.com>
2015-01-22 14:25:02 -0600

    transformation infrastructure and data path installed and working for passThrough (null) transformation, output files match correctly

--- system/management/test2/parameterFiles/testJsonDemo.js
+++ system/management/test2/parameterFiles/testJsonDemo.js
@@ -25,9 +25,11 @@
 {"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/099/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students.txt", "path":"Data", "switches":{"header":false, "append":true}}
 ],
 
-    "process": {
+    "transform": [
+    {
         "type": "passThrough"
-    },
+    }
+    ],
 
     "output": {
         "type": "file",
@@ -35,7 +37,7 @@
             "parentPath": "/Users/tqwhite/testLinkpoint/testDataDest/AlbanyJsonTest/"
         },
         "control": {
-            "append": "true",
+            "append": "false",
             "header": "false"
         }
     }
