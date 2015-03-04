7af4aab9996fe6e329d1c662ae2278395bfe769c
TQ White II <tq@justkidding.com>
2015-03-04 11:24:42 -0600

    added production scripts directory, revised test parameter files

--- system/management/devEnvironment/parameterFiles/testJsonDemo.js
+++ system/management/devEnvironment/parameterFiles/testJsonDemo.js
@@ -2,7 +2,21 @@
     "input": [  
 
 
-{"source":"http://127.0.0.1:8000/uff/1.0/districts/Albany/schools/010/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"}
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/010/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/010/segments/Schedule?sendFlatSpecs=true","destination":"Schedules", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/010/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/011/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/011/segments/Schedule?sendFlatSpecs=true","destination":"Schedules", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/011/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/020/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/020/segments/Schedule?sendFlatSpecs=true","destination":"Schedules", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/020/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/099/segments/Guardian?sendFlatSpecs=true","destination":"Guardians", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/099/segments/Schedule?sendFlatSpecs=true","destination":"Schedules", "path":"Data"},
+{"source":"http://127.0.0.1:8000/uff/1.0/districts/Hawley/schools/099/segments/Student_Enrolled?sendFlatSpecs=true","destination":"Students", "path":"Data"}
+
+
+
 
 ],
 
@@ -11,11 +25,11 @@
     "output": {
         "type": "file",
         "context": {
-            "parentPath": "/Users/tqwhite/testLinkpoint/testDataDest/zztqPlans_Albany/",
-            "header":false,
+            "parentPath": "/Users/tqwhite/testLinkpoint/testDataDest/zzStudentPlans/hawley2/",
+            "header":true,
             "fileExtension":".txt",
             
-            "databaseName":"zztqPlans_Albany",
+            "databaseName":"zzStudentPlans",
 			"authParmsFile":"qubuntuMysqlAuth.json"
         },
         
